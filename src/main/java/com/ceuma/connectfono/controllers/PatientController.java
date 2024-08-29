16f7bf95964e3809b3b467bbe572c55c759c9bd4
nogueira <marcosrafael.nm62@gmail.com>
2024-08-29 20:16:33 -0300

    Update e BuildResponse adicionados

--- src/main/java/com/ceuma/connectfono/controllers/PatientController.java
+++ src/main/java/com/ceuma/connectfono/controllers/PatientController.java
@@ -1,10 +1,12 @@
 package com.ceuma.connectfono.controllers;
 
 import com.ceuma.connectfono.exceptions.patient.BadRequestException;
+import com.ceuma.connectfono.handlers.ErrorResponse;
 import com.ceuma.connectfono.models.Patient;
+import com.ceuma.connectfono.responses.PatientResponse;
 import com.ceuma.connectfono.services.PatientService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @Controller
 @RequestMapping("/patient")
@@ -21,21 +24,36 @@ public class PatientController {
     private PatientService patientService;
 
 
+
+    @GetMapping("")
+    public ResponseEntity<List<Patient>> findAll(){
+        System.out.println("entrou no get all");
+        try{
+            List<Patient> patients = this.patientService.findAll();
+            return ResponseEntity.ok().body(patients);
+        }catch (RuntimeException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
     //depois trocar para Patient, pois ainda nao fiz os handlers
     @GetMapping("/{id}")
     public ResponseEntity<Object> findById(@PathVariable Long id){
         System.out.println("entrou");
         try{
             if(id == null){
-                throw new RuntimeException("Id não pode ser nulo");
+                throw new BadRequestException("Id não pode ser nulo");
             }
             Patient patient = this.patientService.findById(id);
             if(patient == null){
-                throw new RuntimeException("Não existe paciente com esse id");
+                throw new BadRequestException("Não existe paciente com esse id");
             }
             return ResponseEntity.ok().body(patient);
         }catch(RuntimeException e){
-            throw new RuntimeException(e.getMessage());
+            if(e instanceof BadRequestException){
+                throw new BadRequestException(e.getMessage());
+            }
+            return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Um erro desconhecido aconteceu", e);
+            //throw new RuntimeException(e.getMessage());
         }
     }
 
@@ -48,5 +66,33 @@ public class PatientController {
         this.patientService.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> update(@RequestBody Patient obj, @PathVariable Long id){
+        if(obj.getName() == null && obj.getEmail() == null && obj.getDependents() == null && obj.getPassword() == null){
+            throw new BadRequestException("Insira pelo menos um dos campos: Nome, Email, Senha ou Dependentes");
+        }
+        Patient newPatient = this.patientService.update(obj, id);
+
+        return ResponseEntity.ok().body(buildSuccessResponse(200, "paciente alterado com sucesso", HttpStatus.OK, newPatient));
+    }
+
+
+
+    // BUILDRESPONSES abaixo
+    public ResponseEntity<Object> buildSuccessResponse(int status, String message, HttpStatus httpStatus, Patient patient){
+        PatientResponse patientResponse = new PatientResponse(status, message, patient);
+        return ResponseEntity.status(httpStatus).body(patientResponse);
+    }
+    public ResponseEntity<Object> buildSuccessResponse(int status, String message, HttpStatus httpStatus, Patient patient, List<Patient> patients){
+        PatientResponse patientResponse = new PatientResponse(status, message);
+        patientResponse.setPatients(patients);
+        return ResponseEntity.status(httpStatus).body(patientResponse);
+    }
+    public ResponseEntity<Object> buildErrorResponse(HttpStatus httpStatus, String message, Exception exception){
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+        return ResponseEntity.status(httpStatus).body(errorResponse);
     }
 }
--- src/main/java/com/ceuma/connectfono/services/PatientService.java
+++ src/main/java/com/ceuma/connectfono/services/PatientService.java
@@ -1,5 +1,6 @@
 package com.ceuma.connectfono.services;
 
+import com.ceuma.connectfono.exceptions.patient.BadRequestException;
 import com.ceuma.connectfono.models.Patient;
 import com.ceuma.connectfono.repositories.DependentRepository;
 import com.ceuma.connectfono.repositories.PatientRepository;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,11 +20,21 @@ public class PatientService {
     @Autowired
     private DependentRepository dependentRepository;
 
+
+    public List<Patient> findAll(){
+        List<Patient> patients = this.patientRepository.findAll();
+        if(patients == null){
+            throw new BadRequestException("Não há pacientes cadastrados");
+        }
+        return patients;
+
+    }
     public Patient findById(Long id){
         Optional<Patient> patient = patientRepository.findById(id);
-        return patient.orElseThrow(() -> new RuntimeException(
+        return patient.orElseThrow(() -> new BadRequestException(
                 "Paciente não encontrado"
         ));
+
     }
     @Transactional
     public Patient create(Patient obj){
@@ -34,5 +46,26 @@ public class PatientService {
         }
         return obj;
     }
+
+    @Transactional
+    public Patient update(Patient obj, Long id){
+        System.out.println(obj.getCpf());
+        Patient newPatient = findById(id);
+//        if (obj.getCpf() != newPatient.getCpf()){
+//            throw new BadRequestException("Não é possível alterar o CPF");
+//        }
+        if(obj.getCpf() != null){
+            throw new BadRequestException("Não é possível alterar o cpf");
+        }
+        newPatient.setName(obj.getName());
+        newPatient.setDependents(obj.getDependents());
+        newPatient.setEmail(obj.getEmail());
+        newPatient.setPassword(obj.getPassword());
+
+        this.patientRepository.save(newPatient);
+        return newPatient;
+    }
+
+
 
 }
