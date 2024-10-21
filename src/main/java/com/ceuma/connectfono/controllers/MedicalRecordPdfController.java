6bc44d6edb1295c1e28d9a0358b4f1185a517788
nogueira <marcosrafael.nm62@gmail.com>
2024-10-20 21:52:47 -0300

    formatacoes no pdf

--- src/main/java/com/ceuma/connectfono/controllers/MedicalRecordPdfController.java
+++ src/main/java/com/ceuma/connectfono/controllers/MedicalRecordPdfController.java
@@ -4,12 +4,15 @@ import com.aspose.pdf.*;
 import com.aspose.pdf.operators.Do;
 import com.ceuma.connectfono.dto.MedicalRecordDTO;
 import com.ceuma.connectfono.models.MedicalRecord;
+import com.ceuma.connectfono.models.Patient;
+import com.ceuma.connectfono.models.Questions;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.w3c.dom.Text;
 
 import java.io.File;
+import java.util.List;
 
 
 @Getter
@@ -21,28 +24,51 @@ public class MedicalRecordPdfController {
 
     public String generatePdf(MedicalRecordDTO medicalRecord) {
         Document doc = new Document();
+        Patient patient = medicalRecord.getMedicalRecord().getPatient();
+        List<Questions> questionsList = medicalRecord.getMedicalHistory().getQuestions();
 
         Page page = doc.getPages().add();
         page.setPageSize(PageSize.getA4().getWidth(), PageSize.getA4().getHeight());
 
-//        TextFragment title = new TextFragment(medicalRecord.getMedicalRecord().getTitle());
-//        title.setMargin(new MarginInfo(20, 20, 0, 0));
-//        title.setPosition(new Position(20, 20));
-        //title.setText(medicalRecord.getMedicalRecord().getTitle());
-        //title.setText("teste");
 
-        // Add Header
-
-        TextFragment copyRight = new TextFragment("Clínica de FonoAudiologia do CEUMA");
+        //TextFragment copyRight = new TextFragment("Clínica de FonoAudiologia do CEUMA");
 
         page.getParagraphs().add(generateTitle(medicalRecord.getMedicalRecord().getTitle()));
+        page.getParagraphs().add(generateMediumText("data de expedição", aligment.Center));
+        page.getParagraphs().add(generateSmallText(
+                medicalRecord.getMedicalRecord().getDate().toString(), aligment.Center));
+
         page.getParagraphs().add(generateTopic("Dados do Paciente"));
+
         page.getParagraphs().add(generateMediumText("Nome"));
-        page.getParagraphs().add(generateSmallText(medicalRecord.getMedicalRecord().getPatient().getName()));
+        page.getParagraphs().add(generateSmallText(patient.getName(),aligment.Left));
         page.getParagraphs().add(generateMediumText("Cpf"));
-        page.getParagraphs().add(generateSmallText(medicalRecord.getMedicalRecord().getPatient().getCpf()));
+        page.getParagraphs().add(generateSmallText(patient.getCpf(),aligment.Left));
         page.getParagraphs().add(generateMediumText("Email"));
-        page.getParagraphs().add(generateSmallText(medicalRecord.getMedicalRecord().getPatient().getEmail()));
+        page.getParagraphs().add(generateSmallText(patient.getEmail(),aligment.Left));
+        page.getParagraphs().add(generateMediumText("Telefone"));
+        page.getParagraphs().add(generateSmallText(patient.getPhone_number(),aligment.Left));
+
+        page.getParagraphs().add(generateTopic("Motivo"));
+        page.getParagraphs().add(generateSmallText(medicalRecord.getMedicalRecord().getMotive()));
+
+        page.getParagraphs().add(generateTopic("Diagnóstico"));
+        page.getParagraphs().add(generateSmallText(medicalRecord.getMedicalRecord().getDiagnosis(),aligment.Left));
+
+        page.getParagraphs().add(generateTopic("Observações"));
+        page.getParagraphs().add(generateSmallText(medicalRecord.getMedicalRecord().getObservations(),aligment.Left));
+
+
+
+        page.getParagraphs().add(generateTopic("Perguntas"));
+        for (Questions question : questionsList) {
+            page.getParagraphs().add(generateMediumText(question.getTitle()));
+            page.getParagraphs().add(generateSmallText(question.getAnswer(),aligment.Left));
+        }
+
+
+
+
 
         File fileName = new File("medicalRecordsPdf/" + medicalRecord.getMedicalRecord().getTitle() + ".pdf");
 
@@ -56,29 +82,46 @@ public class MedicalRecordPdfController {
     public TextFragment generateTitle(String text){
         TextFragment header = new TextFragment(text.toUpperCase());
         header.getTextState().setFont(FontRepository.findFont("Arial-Bold"));
-        header.getTextState().setFontSize(24);
+        header.getTextState().setFontSize(20);
         header.setHorizontalAlignment (HorizontalAlignment.Center);
-        header.setPosition(new Position(130, 720));
-        header.setMargin(new MarginInfo(0, 20, 0 ,0));
+        header.setVerticalAlignment(VerticalAlignment.Center);
+        //header.setPosition(new Position(120, 760));
+        header.setMargin(new MarginInfo(0, 10, 0 ,0));
         return header;
     }
 
     public TextFragment generateTopic(String text){
         TextFragment topic = new TextFragment(text);
         topic.getTextState().setFont(FontRepository.findFont("Arial-Bold"));
-        topic.getTextState().setFontSize(20);
+        topic.getTextState().setFontSize(18);
         topic.setHorizontalAlignment (HorizontalAlignment.Left);
-        topic.setMargin(new MarginInfo(0, 20, 0, 0));
+        topic.setMargin(new MarginInfo(0, 12, 0, 20));
 
         //patientName.setPosition(new Position(130, 820));
        return topic;
     }
 
+    public TextFragment generateSmallText(String text, aligment aligment){
+        TextFragment smallText = new TextFragment(text);
+        smallText.getTextState().setFont(FontRepository.findFont("Arial"));
+        smallText.getTextState().setFontSize(14);
+        smallText.setHorizontalAlignment (getAlignment(aligment));
+        smallText.setMargin(new MarginInfo(0, 2, 0, 0));
+        return smallText;
+    }
     public TextFragment generateSmallText(String text){
         TextFragment smallText = new TextFragment(text);
         smallText.getTextState().setFont(FontRepository.findFont("Arial"));
         smallText.getTextState().setFontSize(14);
         smallText.setHorizontalAlignment (HorizontalAlignment.Left);
+        smallText.setMargin(new MarginInfo(0, 2, 0, 0));
+        return smallText;
+    }
+    public TextFragment generateMediumText(String text, aligment aligment){
+        TextFragment smallText = new TextFragment(text);
+        smallText.getTextState().setFont(FontRepository.findFont("Arial-Bold"));
+        smallText.getTextState().setFontSize(16);
+        smallText.setHorizontalAlignment (getAlignment(aligment));
         smallText.setMargin(new MarginInfo(0, 2, 0, 0));
         return smallText;
     }
@@ -89,6 +132,26 @@ public class MedicalRecordPdfController {
         smallText.setHorizontalAlignment (HorizontalAlignment.Left);
         smallText.setMargin(new MarginInfo(0, 2, 0, 0));
         return smallText;
+    }
+
+    public enum aligment{
+        Right,
+        Left,
+        Center
+    }
+
+    public int getAlignment(aligment aligment){
+        switch (aligment){
+            case Right:
+                return 3;
+            case Left:
+                return 1;
+            case Center:
+                return 2;
+            default:
+                return 1;
+
+        }
     }
 
 
